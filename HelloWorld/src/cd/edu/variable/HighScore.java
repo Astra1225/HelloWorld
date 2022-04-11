ddf1a23cd5ee00c6f54ae65c8df0930ef9e5dded
admin <admin@YD2-15>
2022-04-11 16:39:26 +0900

    0411

--- HelloWorld/src/cd/edu/variable/HighScore.java
+++ HelloWorld/src/cd/edu/variable/HighScore.java
@@ -6,31 +6,31 @@ public class HighScore {
 
 	public static void main(String[] args) {
 		
-		int Korean, English, Math;
+		int num1, num2, num3;
 		int sum, MaxValue;
 		double avg;
 		
 		Scanner scn = new Scanner(System.in);
 		
-		System.out.println("국어 점수 입력:");
-		Korean = scn.nextInt();
+		System.out.println("첫번째 점수 입력:");
+		num1 = scn.nextInt();
 		
-		System.out.println("영어 점수 입력:");
-		English = scn.nextInt();
+		System.out.println("두번째 점수 입력:");
+		num2 = scn.nextInt();
 		
-		System.out.println("수학 점수 입력:");
-		Math = scn.nextInt();
+		System.out.println("세번째 점수 입력:");
+		num3 = scn.nextInt();
 		
-		sum = Korean + English + Math;
+		sum = num1 + num2 + num3;
 		
 		avg = sum / 3.0;
 		
-		MaxValue = Korean;
-		if( MaxValue < English) {
-			MaxValue = English;
+		MaxValue = num1;
+		if( MaxValue < num2) {
+			MaxValue = num2;
 		}
-		if (MaxValue < Math){
-				MaxValue = Math;
+		if (MaxValue < num3){
+				MaxValue = num3;
 		}
 		
 		System.out.printf("세 과목의 총 합은 %d이고 평균은 %.2f 이고 최고점은 %d이다.", sum, avg, MaxValue);
