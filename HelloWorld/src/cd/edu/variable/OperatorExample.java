6c6f3ecb39f4397d65e3449fa8ff5acd49b7c64f
admin <admin@YD2-15>
2022-04-11 17:35:06 +0900

    0411

---
+++ HelloWorld/src/cd/edu/variable/OperatorExample.java
@@ -0,0 +1,32 @@
+package cd.edu.variable;
+
+public class OperatorExample {
+	public static void main(String[] args) {
+		// 나머지 : %
+		// 6/4 => 몫: 1, 나머지 : 2
+		
+		int result = 6 / 4;
+		int remainder = 6 % 4;
+		
+		int val = 5;
+
+		if(val % 2 == 0) {
+			System.out.println("짝수입니다.");
+		 } else {
+			 System.out.println("홀수입니다.");
+			 
+		 }
+		
+		//75300원을 5만원=> ?개, 1만원 => ?개 5천원 => ?개
+				//1천원 => ?개, 5백원 => ?개, 1백원 => ?개
+				//75300원 5만원 1개, 1만원 2개, 5천원 1개, 5백원 1개, 1백원 3개.
+				
+		
+		int sum = 75300;
+		
+		System.out.println(sum/50000);
+		System.out.println(sum%50000/10000);
+		System.out.println(sum%50000%10000/5000);
+		System.out.println(sum%50000%10000%5000/100);
+	}
+}
